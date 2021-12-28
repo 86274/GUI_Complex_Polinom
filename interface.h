d4ad3bc9b6ba9d0df7f007a5456d587e64c4c973
Vadim <47900641+86274@users.noreply.github.com>
2021-12-28 09:46:49 +0300

    Update interface.h

--- interface.h
+++ interface.h
@@ -12,15 +12,15 @@ class TInterface : public QWidget
     Q_OBJECT
 
 public:
-    TInterface(QWidget *parent = nullptr);
+    TInterface(QWidget *parent = nullptr); 
     ~TInterface();
 
 private slots:
-    void on_rbtn_pressed();
+    void on_rbtn_pressed(); //Метод-обработчик нажатия кнопки вычисления корней  
 
-    void on_vbtn_pressed();
+    void on_vbtn_pressed(); //Метод-обработчик нажатия кнопки вычисления значения полинома
 
-    void on_pbtn_pressed();
+    void on_pbtn_pressed(); //Метод-обработчик нажатия кнопки вывода текстового представления полинома
 
 private:
     Ui::TInterface *ui;
